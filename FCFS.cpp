fd01b1cafe7bf2470afaa494f2b71a31bbd5d0f1
Alessio-Farci <ale.farci31@gmail.com>
2016-10-19 16:53:56 +0200

    Update FCFS.cpp

--- FCFS.cpp
+++ FCFS.cpp
@@ -18,9 +18,11 @@
     for(i=0;i<ProcessCount;i++)
     {
     std::cout << " Insert the burst time of the Process [: " << (i+1) << "]" << std::endl;
+    std::cout << " ";
     std:: cin >> burstTime[i];
 
     std::cout << " Insert the waiting time of the Process [: " << (i+1) << "]" << std::endl;
+    std::cout << " ";
     std:: cin >> WaitingTime[i];
 
     TournaroundTime[i]= burstTime[i] + WaitingTime[i];
