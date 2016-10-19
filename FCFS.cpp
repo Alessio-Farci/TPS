5c3fe15794e40a54dc5c7a1d618542ad50bd9c44
Alessio-Farci <ale.farci31@gmail.com>
2016-10-19 12:26:41 +0200

    Create FCFS.cpp

---
+++ FCFS.cpp
@@ -0,0 +1,34 @@
+//First Come First Served Algorithm
+//Author: Alessio Farci
+
+ #include <iostream>
+ #include <stdlib.h>
+    int main()
+{
+    int i;
+    int ProcessCount;
+
+    std::cout << "Insert the number of process: " << std::endl;
+    std:: cin >> ProcessCount;
+
+    int burstTime[ProcessCount];
+    int WaitingTime[ProcessCount];
+    int TournaroundTime[ProcessCount];
+
+    for(i=0;i<ProcessCount;i++)
+    {
+    std::cout << " Insert the burst time of the Process [: " << (i+1) << "]" << std::endl;
+    std:: cin >> burstTime[i];
+
+    std::cout << " Insert the burst time of the Process [: " << (i+1) << "]" << std::endl;
+    std:: cin >> WaitingTime[i];
+
+    TournaroundTime[i]= burstTime[i] + WaitingTime[i];
+
+    std::cout << "The TournaroundTime of the Process [: " << (i+1) << "] is: " << std::endl;
+    std::cout << " " << TournaroundTime[i] << std::endl;
+    std::cout << std::endl;
+    }
+
+ system("pause");
+}
